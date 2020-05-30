38a6891d365448869690042c4ec88bb7e0db7ce6
techienoise <techienoise@gmail.com>
2020-05-30 18:13:11 +0530

    up

--- homeassistant/components/google_event_list/sensor.py
+++ homeassistant/components/google_event_list/sensor.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import datetime
 import pickle
 import os.path
+import voluptuous as vol
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
